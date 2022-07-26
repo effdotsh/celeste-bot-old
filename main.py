c179fd6e9205fc9dd6a76db1acaf7bf576f34594
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-07-25 23:16:56 -0400

    random actions

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import random
+
 import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
+from pynput.keyboard import Key, Controller
+import time
 
 mon = {'left': 100, 'top': 0, 'width': 900, 'height': 1000}
 
 x_pos, y_pos = 0, 0
+
+
 def get_position(sct):
     screenShot = sct.grab(mon)
     img = Image.frombytes(
@@ -34,12 +40,43 @@ def get_position(sct):
         x, y, w, h = cv2.boundingRect(c)
     cv2.rectangle(box, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-
     # cv2.imshow('mask', box)
     return x, y, h, w
 
+
+def generate_actions():
+    actions = []
+    for horizontal in [Key.left, Key.right, None]:
+        for vertical in [Key.up, Key.down, None]:
+            for key in ['z', 'x', None]:  # Jump, dash, none
+                if key != 'x' and vertical == Key.down:
+                    continue
+                if key is None and vertical is not None:
+                    continue
+
+                actions.append([horizontal, vertical, key])
+    return actions
+
+
+def reset_level(keyboard: Controller):
+    keyboard.press('f')
+    time.sleep(0.2)
+    keyboard.release('f')
+    time.sleep(0.2)
+    keyboard.press('s')
+    time.sleep(0.2)
+    keyboard.release('s')
+    time.sleep(0.4)
+
+
+keyboard = Controller()
+
 best_score = -999999
-if __name__=='__main__':
+last_press = time.time()
+start = time.time()
+actions = generate_actions()
+
+if __name__ == '__main__':
     with mss() as sct:
         while True:
             x_pos, y_pos, _, _ = get_position(sct)
@@ -47,3 +84,18 @@ if __name__=='__main__':
             if score > best_score:
                 best_score = score
             print(best_score)
+            action = random.choice(actions)
+
+            if time.time() - last_press > 0.01:
+                for key in action:
+                    if key is not None:
+                        keyboard.press(key)
+                time.sleep(0.1)
+                for key in action:
+                    if key is not None:
+                        keyboard.release(key)
+                last_press = time.time()
+
+            if time.time() - start > 5:
+                reset_level(keyboard)
+                start = time.time()
