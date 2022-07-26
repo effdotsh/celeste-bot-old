2c32510083172b5559291539e0c8cc5a9c2506ed
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-07-25 22:26:15 -0400

    tracks best score

--- main.py
+++ main.py
@@ -5,47 +5,45 @@ from PIL import Image
 
 mon = {'left': 100, 'top': 0, 'width': 900, 'height': 1000}
 
-with mss() as sct:
-    x = 0
-    y = 0
-    w = 0
-    h = 0
-    while True:
-        screenShot = sct.grab(mon)
-        img = Image.frombytes(
-            'RGBA',
-            (screenShot.width, screenShot.height),
-            screenShot.bgra,
-        )
-        # cv2.imshow('test', np.array(img))
-        img = np.array(img)
+x_pos, y_pos = 0, 0
+def get_position(sct):
+    screenShot = sct.grab(mon)
+    img = Image.frombytes(
+        'RGBA',
+        (screenShot.width, screenShot.height),
+        screenShot.bgra,
+    )
+    # cv2.imshow('test', np.array(img))
+    img = np.array(img)
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Threshold of blue in HSV space
-        lower_skin = np.array([2, 75, 255])
-        upper_skin = np.array([22, 95, 255])
+    # Threshold of blue in HSV space
+    lower_skin = np.array([2, 75, 255])
+    upper_skin = np.array([22, 95, 255])
 
-        mask = cv2.inRange(hsv, lower_skin, upper_skin)
+    mask = cv2.inRange(hsv, lower_skin, upper_skin)
+
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    box = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    x, y = x_pos, y_pos
+    w, h = 0, 0
+    for c in cnts:
+        x, y, w, h = cv2.boundingRect(c)
+    cv2.rectangle(box, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 
-        cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        print('---------------')
-        box = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    # cv2.imshow('mask', box)
+    return x, y, h, w
 
-        for c in cnts:
-            x, y, w, h = cv2.boundingRect(c)
-            print(x, y, w, h)
-        cv2.rectangle(box, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # ROI = img[y:y + h, x:x + w]
-            # cv2.imwrite('Image_{}.png'.format(ROI_number), ROI)
-            # ROI_number += 1
-
-        cv2.imshow('mask', box)
-
-        if cv2.waitKey(33) & 0xFF in (
-                ord('q'),
-                27,
-        ):
-            break
+best_score = -999999
+if __name__=='__main__':
+    with mss() as sct:
+        while True:
+            x_pos, y_pos, _, _ = get_position(sct)
+            score = -y_pos
+            if score > best_score:
+                best_score = score
+            print(best_score)
