cf3068f20de7e3305696b0f2ba31d5eab116a699
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2022-07-26 21:16:14 -0400

    added basic targets infrastructure for pop

--- agent.py
+++ agent.py
@@ -1,56 +1,40 @@
 import copy
+from target import Target
 import random
 import numpy as np
 
 
-# insertion, deletion, change
-MUTATION_WEIGHTS = [1, 1, 1]
-
-
-def fill_actions_list(actions: list, num_choices: int, num_actions: int):
-    while len(actions) < num_actions:
-        actions.append(random.randint(0, num_choices - 1))
-
-
 class Agent:
-    def __init__(self, num_choices: int, num_actions: int, mutation_chance: float):
-        self.num_actions = num_actions
-        self.num_choices = num_choices
-        self.actions = []
-        fill_actions_list(self.actions, num_actions=num_actions, num_choices=num_choices)
+    def __init__(self, mutation_chance: float):
+        self.targets = []
+
         self.fitness = 0
         self.run_data = {}
         self.mutation_chance = mutation_chance
+
+    def add_target(self, num_actions, num_choices):
+        target = Target(num_actions=num_actions, num_choices=num_choices)
+        self.targets.append(target)
 
     def get_run_data(self):
         return self.run_data
 
     def get_actions(self):
-        return self.actions[:self.num_actions]
+        actions = []
+        for target in self.targets:
+            actions += target.get_actions()
+        return actions
 
     def set_fitness(self, f):
         self.fitness = f
 
     def clone_from(self, other):
-        self.actions = copy.deepcopy(other.actions)
-        # self.fitness = other.fitness
+        self.targets = copy.deepcopy(other.targets)
+        # for i in range(len(other.targets)):
+        #     self.argets.appned(copy.deepcopy(other.targets[i]))
 
     def mutate(self):
-        for e, a in enumerate(self.actions):
-            if random.random() > self.mutation_chance:
-                continue
-            mut_type = random.choices(range(len(MUTATION_WEIGHTS)), weights=MUTATION_WEIGHTS)[0]
-            # print(mut_type)
-
-            new_action = random.randint(0, self.num_choices - 1)
-
-            if mut_type == 0:  # insertion
-                self.actions.insert(e, new_action)
-            elif mut_type == 1:  # deletion
-                self.actions.pop(e)
-            elif mut_type == 2:  # change
-                self.actions[e] = new_action
-        fill_actions_list(self.actions, self.num_choices, self.num_actions)
+        self.targets[-1].mutate(self.mutation_chance)
 
     def get_fitness(self):
         return self.fitness
--- population_manager.py
+++ population_manager.py
@@ -11,9 +11,6 @@ MIN_MUTATION_CHANCE = 0.05
 MAX_MUTATION_CHANCE = 0.3
 
 
-
-
-
 class Population:
     def __init__(self, population_size, agent_num_actions: int, agent_num_choices: int, write_results: bool = True):
         self.write_results = write_results
@@ -22,15 +19,19 @@ class Population:
 
         mutation_spread = np.linspace(MIN_MUTATION_CHANCE, MAX_MUTATION_CHANCE, population_size)
         for i in range(population_size):
-            a = Agent(num_choices=agent_num_choices, num_actions=agent_num_actions, mutation_chance=mutation_spread[i])
+            a = Agent(mutation_chance=mutation_spread[i])
             self.agents.append(a)
 
         self.results = []
         self.dump_results()
 
+    def add_target(self, num_actions, num_choices):
+        for agent in self.get_agents():
+            agent.add_target(num_actions=num_actions, num_choices=num_choices)
+
     def dump_results(self):
         with open("results.json", 'w+') as f:
-            f.write(json.dumps({"results": self.results}, indent = 4))
+            f.write(json.dumps({"results": self.results}, indent=4))
 
     def get_agents(self):
         return self.agents
@@ -55,7 +56,7 @@ class Population:
                 "generation": self.generation_counter,
                 "fitness": self.agents[0].fitness,
                 "actions": self.agents[0].get_actions(),
-                "run_data":self.agents[0].get_run_data()
+                "run_data": self.agents[0].get_run_data()
             })
             self.dump_results()
 
